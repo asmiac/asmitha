5957a759a68cb7011ddc950afea611058641a953
asmiac <138563072+asmiac@users.noreply.github.com>
2023-07-04 13:11:45 +0530

    1st comit

---
+++ main.py
@@ -0,0 +1,169 @@
+
+# names = "adlin"
+# rollno = 8
+# marks = 80.6
+# car = "false"
+#
+# print(type(names))
+# print(type(rollno))
+# print(type(marks))
+#
+# s = 5j
+# subjects.extend(["oops","python","java"])
+# subjects.remove("tamil")
+# subjects = [s.upper() for s in subjects]
+# print(subjects)
+
+#
+# x, y, z = "peacock", "dove", "parrot"
+# print(x)
+# print(y)
+# print(z)
+
+# c = 7j;
+
+# x = 2
+# y = 5
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+
+# flowers = ["rose", "tulip", "sunflower", "jasmine", "lotus", "hibiscus", "lily", "waterlily", "marigold", "daisy"]
+# print(flowers[5])
+
+# names = ("adlin", "achu", "asmi", "jeri")
+# for x in names:
+#     print(x)
+
+
+# a = 25
+# b = 32
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+# x = 23
+# y = "shogina"
+# z = 3j
+# s = 40.8
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(s))
+
+
+#
+# for j in range (101):
+#     print(j)
+
+# for i in range (1001):
+#     print(i)
+
+
+
+# start = 4
+# end = 19
+# for num in range(start, end + 1):
+#     if num % 2 != 0:
+#         print(num)
+#
+# student = {"name" : "asmi", "rollno" : 34, "marks" : 85}
+# print(student["name"])
+# print(student["rollno"])
+# print(student["marks"])
+# print(len(student))
+
+# electric =False
+# print(type(electric))
+from typing import Dict, Union, Any, re
+
+
+# fruits = {"apple" : 40, "orange" : 35, "pineapple" : 50, "watermelon" : 55, "banana" : 35, "cherry" : 30, "dragonfruit" : 100,
+#           "guava" : 40, "pear" : 45, "rambutan" : 80, "sapota" : 48, "blackberry" : 65, "blueberry" : 75, "papaya" : 55, "jackfruit" : 65,
+#           "starapple" : 90, "gooseberry" : 35, "grapes" : 75, "muskmelon" : 99, "avocado" : 90}
+# fruits.update({"apple" : 55, "watermelon" : 65, "muskmelon" : 88, "guava" : 66})
+# print(fruits)
+# for x in fruits:
+#     print(x)
+# fruits = [f.upper() for f in fruits]
+# print(fruits)
+
+# name = "raju"
+# age = 30
+# if age >= 18:
+#     print("raju is eligible")
+# else:
+#     print("raju is not eligible")
+#
+# name = "abi"
+# age = 22
+# if age >= 18:
+#     print("abi is eligible")
+# else:
+#     print("abi is not eligible")
+
+
+# txt = "the best thing is life are free"
+# print("free" in txt)
+
+#
+# b = "hey, siri"
+# print(b[-4:-2])
+#
+# txt = "It's alright."
+# print(txt)
+
+
+
+# txt = "\141\163\155\151\164\150\141"
+# print(txt)
+
+
+
+# def cal_average(list):
+#     return sum(list) / len(list)
+# list = [15, 2, 5, 8, 9]
+# average = cal_average(list)
+# print('average of the list =', round(average, 2))
+
+#
+# def Count_vowels(string, vowels):
+#     final = [each for each in string if each in vowels]
+#     print(len(final))
+#     print(final)
+# string = "My name is Asmitha"
+# vowels = "AaEeIiOoUu"
+# Count_vowels(string, vowels);
+#
+#
+# def count_vowels(string, vowels):
+#     string = string.casefold()
+#     count = {}.fromkeys(vowels, 0)
+#     for character in string:
+#         if character in count:
+#             count[character] += 1
+#     return count
+# vowels = 'aeiou'
+# string = 'my name is asmitha'
+# print(count_vowels(string, vowels))
+
+#
+
+
+# def find_max(a, b, c):
+#     if (a >= b) and (a >= c):
+#         largest = a
+#     elif (b >= a) and (b >= c):
+#         largest = b
+#     else:
+#         largest = c
+#     return largest
+# print(find_max(20, 50, 80))
+
+
